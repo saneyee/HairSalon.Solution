c02f90b4f7ba149efde2c5c42a07d75ccc01f3c2
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-27 16:25:34 -0700

    it adds and updates the Stylist.cs file

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -64,37 +64,6 @@ namespace HairSalon.Models
             return allStylists;
         }
 
-        public List<Client> GetClients()
-        {
-            List<Client> allStylistClients = new List<Client> {};
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
-            MySqlParameter stylistId = new MySqlParameter();
-            stylistId.ParameterName = "@stylist_id";
-            stylistId.Value = this._id;
-            cmd.Parameters.Add(stylistId);
-
-
-            var rdr = cmd.ExecuteReader() as MySqlDataReader;
-            while(rdr.Read())
-            {
-              int clientId = rdr.GetInt32(0);
-              string clientName = rdr.GetString(1);
-              int clientStylistId = rdr.GetInt32(2);
-
-              Client newClient = new Client(clientName, clientStylistId, clientId);
-              allStylistClients.Add(newClient);
-            }
-            conn.Close();
-            if (conn != null)
-            {
-                conn.Dispose();
-            }
-            return allStylistClients;
-        }
-
         public void Save()
         {
             MySqlConnection conn = DB.Connection();
@@ -159,6 +128,36 @@ namespace HairSalon.Models
             {
                 conn.Dispose();
             }
+        }
+        public List<Client> GetClients()
+        {
+            List<Client> allStylistClients = new List<Client> {};
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
+            MySqlParameter stylistId = new MySqlParameter();
+            stylistId.ParameterName = "@stylist_id";
+            stylistId.Value = this._id;
+            cmd.Parameters.Add(stylistId);
+
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+              int clientId = rdr.GetInt32(0);
+              string clientName = rdr.GetString(1);
+              int clientStylistId = rdr.GetInt32(2);
+
+              Client newClient = new Client(clientName, clientStylistId, clientId);
+              allStylistClients.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allStylistClients;
         }
     }
 }
