10e52fb9162e9d6650e3ba6ba11c97a38296c735
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-27 17:30:58 -0700

    first test passes for stylist

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -20,7 +20,7 @@ namespace HairSalon.Tests
         }
 
        [TestMethod]
-       public void GetAll_CategoriesEmptyAtFirst_0()
+       public void GetAll_StylistsEmptyAtFirst_0()
        {
          //Arrange, Act
          int result = Stylist.GetAll().Count;
@@ -29,82 +29,82 @@ namespace HairSalon.Tests
          Assert.AreEqual(0, result);
        }
 
-      [TestMethod]
-      public void Equals_ReturnsTrueForSameName_Stylist()
-      {
-        //Arrange, Act
-        Stylist firstStylist = new Stylist("Mexican");
-        Stylist secondStylist = new Stylist("Mexican");
-
-        //Assert
-        Assert.AreEqual(firstStylist, secondStylist);
-      }
-
-      [TestMethod]
-      public void Save_SavesStylistToDatabase_StylistList()
-      {
-        //Arrange
-        Stylist testStylist = new Stylist("Mexican");
-        testStylist.Save();
-
-        //Act
-        List<Stylist> result = Stylist.GetAll();
-        List<Stylist> testList = new List<Stylist>{testStylist};
-
-        //Assert
-        CollectionAssert.AreEqual(testList, result);
-      }
-
-
-     [TestMethod]
-     public void Save_DatabaseAssignsIdToStylist_Id()
-     {
-       //Arrange
-       Stylist testStylist = new Stylist("Mexican");
-       testStylist.Save();
-
-       //Act
-       Stylist savedStylist = Stylist.GetAll()[0];
-
-       int result = savedStylist.GetId();
-       int testId = testStylist.GetId();
-
-       //Assert
-       Assert.AreEqual(testId, result);
-    }
-
-
-    [TestMethod]
-    public void Find_FindsStylistInDatabase_Stylist()
-    {
-      //Arrange
-      Stylist testStylist = new Stylist("Mexican");
-      testStylist.Save();
-
-      //Act
-      Stylist foundStylist = Stylist.Find(testStylist.GetId());
-
-      //Assert
-      Assert.AreEqual(testStylist, foundStylist);
-    }
-
-    [TestMethod]
-    public void GetClients_RetrievesAllClientsWithStylist_ClientList()
-    {
-      Stylist testStylist = new Stylist("Mexican");
-      testStylist.Save();
-
-      Client firstClient = new Client("Mexican", testStylist.GetId());
-      firstClient.Save();
-      Client secondClient = new Client("Japanese", testStylist.GetId());
-      secondClient.Save();
-
-
-      List<Client> testClientList = new List<Client> {firstClient, secondClient};
-      List<Client> resultClientList = testStylist.GetClients();
-
-      CollectionAssert.AreEqual(testClientList, resultClientList);
-    }
+    //   [TestMethod]
+    //   public void Equals_ReturnsTrueForSameName_Stylist()
+    //   {
+    //     //Arrange, Act
+    //     Stylist firstStylist = new Stylist("Anna");
+    //     Stylist secondStylist = new Stylist("Anna");
+    //
+    //     //Assert
+    //     Assert.AreEqual(firstStylist, secondStylist);
+    //   }
+    //
+    //   [TestMethod]
+    //   public void Save_SavesStylistToDatabase_StylistList()
+    //   {
+    //     //Arrange
+    //     Stylist testStylist = new Stylist("Anna");
+    //     testStylist.Save();
+    //
+    //     //Act
+    //     List<Stylist> result = Stylist.GetAll();
+    //     List<Stylist> testList = new List<Stylist>{testStylist};
+    //
+    //     //Assert
+    //     CollectionAssert.AreEqual(testList, result);
+    //   }
+    //
+    //
+    //  [TestMethod]
+    //  public void Save_DatabaseAssignsIdToStylist_Id()
+    //  {
+    //    //Arrange
+    //    Stylist testStylist = new Stylist("Anna");
+    //    testStylist.Save();
+    //
+    //    //Act
+    //    Stylist savedStylist = Stylist.GetAll()[0];
+    //
+    //    int result = savedStylist.GetId();
+    //    int testId = testStylist.GetId();
+    //
+    //    //Assert
+    //    Assert.AreEqual(testId, result);
+    // }
+    //
+    //
+    // [TestMethod]
+    // public void Find_FindsStylistInDatabase_Stylist()
+    // {
+    //   //Arrange
+    //   Stylist testStylist = new Stylist("Anna");
+    //   testStylist.Save();
+    //
+    //   //Act
+    //   Stylist foundStylist = Stylist.Find(testStylist.GetId());
+    //
+    //   //Assert
+    //   Assert.AreEqual(testStylist, foundStylist);
+    // }
+    //
+    // [TestMethod]
+    // public void GetClients_RetrievesAllClientsWithStylist_ClientList()
+    // {
+    //   Stylist testStylist = new Stylist("Anna");
+    //   testStylist.Save();
+    //
+    //   Client firstClient = new Client("Anna", testStylist.GetId());
+    //   firstClient.Save();
+    //   Client secondClient = new Client("Japanese", testStylist.GetId());
+    //   secondClient.Save();
+    //
+    //
+    //   List<Client> testClientList = new List<Client> {firstClient, secondClient};
+    //   List<Client> resultClientList = testStylist.GetClients();
+    //
+    //   CollectionAssert.AreEqual(testClientList, resultClientList);
+    // }
 
   }
 }
