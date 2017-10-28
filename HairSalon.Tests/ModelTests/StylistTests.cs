bc9a0a24473ba036148246649b93047c8a9422ef
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-27 17:00:28 -0700

    it adds the databases

---
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -0,0 +1,110 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class StylistTests : IDisposable
+  {
+        public StylistTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889 ;database=saneyee_sarkar_test;";
+        }
+
+        public void Dispose()
+        {
+          Client.DeleteAll();
+          Stylist.DeleteAll();
+        }
+
+       [TestMethod]
+       public void GetAll_CategoriesEmptyAtFirst_0()
+       {
+         //Arrange, Act
+         int result = Stylist.GetAll().Count;
+
+         //Assert
+         Assert.AreEqual(0, result);
+       }
+
+      [TestMethod]
+      public void Equals_ReturnsTrueForSameName_Stylist()
+      {
+        //Arrange, Act
+        Stylist firstStylist = new Stylist("Mexican");
+        Stylist secondStylist = new Stylist("Mexican");
+
+        //Assert
+        Assert.AreEqual(firstStylist, secondStylist);
+      }
+
+      [TestMethod]
+      public void Save_SavesStylistToDatabase_StylistList()
+      {
+        //Arrange
+        Stylist testStylist = new Stylist("Mexican");
+        testStylist.Save();
+
+        //Act
+        List<Stylist> result = Stylist.GetAll();
+        List<Stylist> testList = new List<Stylist>{testStylist};
+
+        //Assert
+        CollectionAssert.AreEqual(testList, result);
+      }
+
+
+     [TestMethod]
+     public void Save_DatabaseAssignsIdToStylist_Id()
+     {
+       //Arrange
+       Stylist testStylist = new Stylist("Mexican");
+       testStylist.Save();
+
+       //Act
+       Stylist savedStylist = Stylist.GetAll()[0];
+
+       int result = savedStylist.GetId();
+       int testId = testStylist.GetId();
+
+       //Assert
+       Assert.AreEqual(testId, result);
+    }
+
+
+    [TestMethod]
+    public void Find_FindsStylistInDatabase_Stylist()
+    {
+      //Arrange
+      Stylist testStylist = new Stylist("Mexican");
+      testStylist.Save();
+
+      //Act
+      Stylist foundStylist = Stylist.Find(testStylist.GetId());
+
+      //Assert
+      Assert.AreEqual(testStylist, foundStylist);
+    }
+
+    [TestMethod]
+    public void GetClients_RetrievesAllClientsWithStylist_ClientList()
+    {
+      Stylist testStylist = new Stylist("Mexican");
+      testStylist.Save();
+
+      Client firstClient = new Client("Mexican", testStylist.GetId());
+      firstClient.Save();
+      Client secondClient = new Client("Japanese", testStylist.GetId());
+      secondClient.Save();
+
+
+      List<Client> testClientList = new List<Client> {firstClient, secondClient};
+      List<Client> resultClientList = testStylist.GetClients();
+
+      CollectionAssert.AreEqual(testClientList, resultClientList);
+    }
+
+  }
+}
